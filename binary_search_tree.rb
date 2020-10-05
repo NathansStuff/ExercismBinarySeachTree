8a282be6487d2c591da042e156d433d9bf53811a
NathansStuff <63134709+NathansStuff@users.noreply.github.com>
2020-10-05 15:42:10 +1100

    First test

--- binary_search_tree.rb
+++ binary_search_tree.rb
@@ -1,8 +1,9 @@
-=begin
-Write your code for the 'Binary Search Tree' exercise in this file. Make the tests in
-`binary_search_tree_test.rb` pass.
+class Bst
+    def initialize(n)
+        @n=n
+    end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/binary-search-tree` directory.
-=end
-
+    def data
+        return @n
+    end
+end
